56c8048e804ee007ecabf8bcb1692dce2cab6a0f
kavehzare98 <66182975+kavehzare98@users.noreply.github.com>
2025-05-02 21:59:57 -0700

    implemented a lot of the game class

--- src/model/game.py
+++ src/model/game.py
@@ -1,25 +1,10 @@
 '''
 GAME CLASS:
-===========
-ATTRIBUTES
-    + possible_moves : list(string)
-
-METHODS
-    + get_default_grid : list(string)
-    + get_menu_grid : list(string)
-    + get_current_grid : list(string)
-    + get_possible_moves : list(string)
-
-    + set_default_grid(new_default : list(string) ) : void
-    + set_menu_grid(new_menu : list(string) ) : void
-    + set_current_grid(new_curr : list(string) ) : void
-    + set_possible_moves(new_moves : list(string) ) : void
-
-    + valid_move(move : int)  : bool
     + update_current_grid(move : int, symbol : string) : void
     + update_possible_moves(move : int) : void
     + is_draw() : bool
     + find_winner() : string
+    + reset_game() : void
 
 '''
 
@@ -30,4 +15,36 @@ class Game:
         self.current_grid = self.default_grid
         self.possible_moves = self.menu_grid
 
-    def 
+    # GETTERS
+    def get_default_grid(self) -> list:
+        return self.default_grid
+    
+    def get_menu_grid(self) -> list:
+        return self.menu_grid
+    
+    def get_current_grid(self) -> list:
+        return self.current_grid
+    
+    def get_default_grid(self) -> list:
+        return self.possible_moves
+    
+    # SETTERS
+    def set_default_grid(self, new_grid : list) -> None:
+        self.default_grid = new_grid
+
+    def set_menu_grid(self, new_grid : list) -> None:
+        self.menu_grid = new_grid
+
+    def set_current_grid(self, new_grid : list) -> None:
+        self.current_grid = new_grid
+        
+    def set_possible_moves(self, new_moves : list) -> None:
+        self.possible_moves = new_moves
+
+    # SPECIAL METHODS
+    def validate_move(self, move : int) -> bool:
+        if move in self.possible_moves:
+            return True
+        return False
+    
+    def update_current_grid(move : int, symbol : string) : void
